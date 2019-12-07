e01f6a4f0658beeecce82dc2a09685bc5cd856b8
Kirill Artihovich <new.shtirlic@mail.ru>
2019-12-07 20:48:32 +0300

    ~ build

---
+++ build/precache-manifest.1e61be183dcb288a82359b601c5741fa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcba98af205ffe63165a3c1d38caa8b1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5eb51eaee2ca70b6363",
+    "url": "./static/css/main.efb2dd14.chunk.css"
+  },
+  {
+    "revision": "cd0524bd2f38f264dea7",
+    "url": "./static/js/2.e404a141.chunk.js"
+  },
+  {
+    "revision": "a5eb51eaee2ca70b6363",
+    "url": "./static/js/main.e1ec53b6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
